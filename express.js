23d6f226ec7a277effd70a28971bb7d7f80a4eaf
YARAMANENI RAHUL <rahulyaramaneni@gmail.com>
2025-03-05 00:16:52 +0530

    sqlite

--- express.js
+++ express.js
@@ -1,10 +1,10 @@
 const express=require("express");
 const app=express();
 
-app.get("/",(request,response)=>{
-    console.log("Hello World");
-    response.send("Hello World");
-});
+app.get("/date", (request, response) => {
+    let date = new Date();
+    response.send(`Today's date is ${date}`);
+  });
 app.listen(3000,() => {
     console.log("Server is running on http://localhost:3000");
 });

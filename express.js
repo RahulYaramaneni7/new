d5a135b23897a65be8268bf16759df667d6fe659
YARAMANENI RAHUL <rahulyaramaneni@gmail.com>
2025-03-05 00:10:52 +0530

    remove rma

--- express.js
+++ express.js
@@ -3,5 +3,8 @@ const app=express();
 
 app.get("/",(request,response)=>{
     console.log("Hello World");
+    response.send("Hello World");
 });
-app.listen(3000);
+app.listen(3000,() => {
+    console.log("Server is running on http://localhost:3000");
+});

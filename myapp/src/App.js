04a50252cc7cf04aab913082ee0ac6a125448669
YARAMANENI RAHUL <rahulyaramaneni@gmail.com>
2025-05-23 12:20:23 +0530

    all files

---
+++ myapp/src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+import {Route,Switch} from 'react-router-dom'
+
+
+const App=()=>{
+  <Switch>
+    <Route exact path="/login" component={}/>
+  </Switch>
+}
+
+export default App;
